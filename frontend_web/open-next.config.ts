e1128693c4494d334c3f6f20d2ba0654f650a00d
Heroho <chh6518@gmail.com>
2026-03-07 01:43:27 +0900

    fix: Remove unused kvCache from open-next.config.ts to fix Cloudflare build

--- frontend_web/open-next.config.ts
+++ frontend_web/open-next.config.ts
@@ -1,11 +1,8 @@
-import cache from "@opennextjs/cloudflare/kvCache";
-
 export default {
     default: {
         override: {
             wrapper: "cloudflare-node",
             converter: "edge",
-            incrementalCache: async () => cache,
             tagCache: "dummy",
             queue: "dummy",
         },
